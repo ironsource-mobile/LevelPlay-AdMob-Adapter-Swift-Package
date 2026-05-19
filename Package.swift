b5612f6f95be2296db6f947e15f24b8b7021111c
UnityLevelPlay <svc-levelplay-release@unity3d.com>
2026-05-19 09:17:02 +0000

    Release 5.9.0 - SDK 13.4.0

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     .library(name: "AdMobAdapter", targets: ["AdMobAdapter"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads", exact: "13.3.0"),
+    .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads", exact: "13.4.0"),
     .package(url: "https://github.com/ironsource-mobile/Unity-Mediation-iAds-Swift-Package", "9.2.0"..<"10.0.0"),
   ],
   targets: [
@@ -22,8 +22,8 @@ let package = Package(
     ),
     .binaryTarget(
       name: "AdMobAdapterSDK",
-      url: "https://raw.githubusercontent.com/ironsource-mobile/iOS-adapters/master/admob-adapter/5.8.0/ISAdMobAdapter5.8.0.zip",
-      checksum: "5f071bbf186b3a446c3d5fe7dbcf0b2b5a7eea61bfe4cfbd2778a83c6ea775a2"
+      url: "https://raw.githubusercontent.com/ironsource-mobile/iOS-adapters/master/admob-adapter/5.9.0/ISAdMobAdapter5.9.0.zip",
+      checksum: "9a0cad3662b4000e1bdd011d5ff29a74fc7ab722c0223d8781e2cbd6ba70efe8"
     )
   ]
 )
